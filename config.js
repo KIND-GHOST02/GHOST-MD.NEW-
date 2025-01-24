57da63077dc6b7172aa9a3c83367bb6d50e43ad6
CYBER GHOST <chinthakashamin0@gmail.com>
2025-01-24 22:29:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "LBZWUB4B#m6LT6Zyszur1JS5QzM2kfsHVcAhLlBpXa6nAvMvdSRI",
+    SESSION_ID: process.env.SESSION_ID || "uFIBiByS#BMD00YZYCzDU3EsPSKus5gD9qdTiq3NhNQ5HwMtQ0DA",
     CAPTION: process.env.CAPTION || "POWERED BY GHOST MD",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
     READ_MESSAGE: process.env.READ_MESSAGE || "false", // Added auto-read configuration
